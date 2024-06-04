6e3e51b485855f830ff1db67970a952fdd605eaa
DeweySalt <76853940+Deweh@users.noreply.github.com>
2024-06-03 23:09:55 -0400

    change context display styling

--- src/js/memory.js
+++ src/js/memory.js
@@ -7,6 +7,32 @@ import { AppSettings, SettingFields } from "./settings";
 import { GetStoryText } from ".";
 
 export class Memory extends React.Component{
+    ctx = {
+        maxCount: 0,
+        storyCount: 0,
+        promptCount: 0,
+        totalCount: 0,
+        storyPercent: 0,
+        promptPercent: 0,
+        totalPercent: 0
+    }
+
+    constructor(props){
+        super(props);
+
+        let prompt = AppSettings[SettingFields.PromptFormat];
+        prompt = prompt.replace("{system}", AppSettings[SettingFields.SystemPrompt]);
+        prompt = prompt.replace("{input}", "");
+
+        this.ctx.maxCount = AppSettings[SettingFields.ContextLimit];
+        this.ctx.storyCount = this.getTokenCount(GetStoryText());
+        this.ctx.promptCount = this.getTokenCount(prompt);
+        this.ctx.totalCount = this.ctx.storyCount + this.ctx.promptCount;
+
+        this.ctx.storyPercent = ((this.ctx.storyCount / this.ctx.maxCount) * 100);
+        this.ctx.promptPercent = ((this.ctx.promptCount / this.ctx.maxCount) * 100);
+        this.ctx.totalPercent = ((this.ctx.totalCount / this.ctx.maxCount) * 100);
+    }
 
     getTokenCount = (str) => {
         switch(AppSettings[SettingFields.Tokenizer]){
@@ -22,30 +48,18 @@ export class Memory extends React.Component{
     }
 
     render(){
-        let prompt = AppSettings[SettingFields.PromptFormat];
-        prompt = prompt.replace("{system}", AppSettings[SettingFields.SystemPrompt]);
-        prompt = prompt.replace("{input}", "");
-
-        let maxCount = AppSettings[SettingFields.ContextLimit];
-        let storyCount = this.getTokenCount(GetStoryText());
-        let promptCount = this.getTokenCount(prompt);
-        let totalCount = storyCount + promptCount;
-
-        let storyPercent = ((storyCount / maxCount) * 100);
-        let promptPercent = ((promptCount / maxCount) * 100);
-
         return(
             <div className="options-container">
                 <div className="option-field">
-                    <span>Context Usage: {totalCount}/{maxCount} Tokens ({Math.round((totalCount / maxCount) * 100)}%)</span>
+                    <span>Context Usage: {this.ctx.totalCount}/{this.ctx.maxCount} Tokens ({Math.round(this.ctx.totalPercent)}%)</span>
                     <div class="progress-bar flex-horizontal">
-                        <div style={{width: promptPercent.toString() + "%", display: "inline"}}>
-                            <Tooltip text={"System Prompt: " + promptCount.toString() + " Tokens (" + Math.round(promptPercent).toString() + "%)"}>
-                                <div class="progress-fill yellow"></div>
+                        <div style={{width: this.ctx.promptPercent.toString() + "%", display: "inline"}}>
+                            <Tooltip text={"System Prompt: " + this.ctx.promptCount.toString() + " Tokens (" + Math.round(this.ctx.promptPercent).toString() + "%)"}>
+                                <div class="progress-fill yellow first"></div>
                             </Tooltip>
                         </div>
-                        <div style={{width: storyPercent.toString() +  "%", display: "inline"}}>
-                            <Tooltip text={"Story: " + storyCount.toString() + " Tokens (" + Math.round(storyPercent).toString() + "%)"}>
+                        <div style={{width: this.ctx.storyPercent.toString() +  "%", display: "inline"}}>
+                            <Tooltip text={"Story: " + this.ctx.storyCount.toString() + " Tokens (" + Math.round(this.ctx.storyPercent).toString() + "%)"}>
                                 <div class="progress-fill"></div>
                             </Tooltip>
                         </div>
